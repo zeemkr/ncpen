c903a9ab602579dffd2c41363ecf40a22fadf1e9
Dongshin Kim <dongshin.kim@live.com>
2018-02-14 14:52:07 -0800

    Travis setup for Ghostscript

--- Build utils.R
+++ Build utils.R
@@ -3,11 +3,15 @@ roxygen2::roxygenize('.', roclets=c('rd', 'collate', 'namespace', 'vignette'));
 if(file.exists("./inst/doc/ncpen.pdf")) file.remove("./inst/doc/ncpen.pdf");
 system("R CMD Rd2pdf -o ./inst/doc/ncpen.pdf .");
 
-# To resolve dll registration warnings.
+# To resolve dll registration warnings. -------------------------------
 # tools::package_native_routine_registration_skeleton(".");
 # This generates codes for init.c.
 # Run it and copy & paste to init.c
 tools::package_native_routine_registration_skeleton(".", character_only = FALSE)
+
+
+# To use Travis ----------------------------
+devtools::use_travis();
 
 
 # R CMD check
