2d678bf7321d9ff55eb665c05d9359d043d77c82
Dongshin Kim <dongshin.kim@live.com>
2018-07-12 11:00:50 -0700

    2018 summer update

--- src/ncpen_init.c
+++ src/ncpen_init.c
@@ -4,20 +4,22 @@
 #include <R_ext/Rdynload.h>
 
 /* FIXME:
-Check these declarations against the C/Fortran source code.
-*/
+ Check these declarations against the C/Fortran source code.
+ */
 
 /* .Call calls */
 extern SEXP _ncpen_native_cpp_ncpen_fun_(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP _ncpen_native_cpp_obj_fun_(SEXP, SEXP, SEXP, SEXP, SEXP);
-extern SEXP _ncpen_native_cpp_obj_grad_fun_(SEXP, SEXP, SEXP, SEXP, SEXP);
+extern SEXP _ncpen_native_cpp_obj_fun_(SEXP, SEXP, SEXP, SEXP);
+extern SEXP _ncpen_native_cpp_obj_grad_fun_(SEXP, SEXP, SEXP, SEXP);
+extern SEXP _ncpen_native_cpp_obj_hess_fun_(SEXP, SEXP, SEXP, SEXP);
 extern SEXP _ncpen_native_cpp_p_ncpen_fun_(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 extern SEXP _ncpen_native_cpp_qlasso_fun_(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
 
 static const R_CallMethodDef CallEntries[] = {
      {"_ncpen_native_cpp_ncpen_fun_",    (DL_FUNC) &_ncpen_native_cpp_ncpen_fun_,    17},
-     {"_ncpen_native_cpp_obj_fun_",      (DL_FUNC) &_ncpen_native_cpp_obj_fun_,       5},
-     {"_ncpen_native_cpp_obj_grad_fun_", (DL_FUNC) &_ncpen_native_cpp_obj_grad_fun_,  5},
+     {"_ncpen_native_cpp_obj_fun_",      (DL_FUNC) &_ncpen_native_cpp_obj_fun_,       4},
+     {"_ncpen_native_cpp_obj_grad_fun_", (DL_FUNC) &_ncpen_native_cpp_obj_grad_fun_,  4},
+     {"_ncpen_native_cpp_obj_hess_fun_", (DL_FUNC) &_ncpen_native_cpp_obj_hess_fun_,  4},
      {"_ncpen_native_cpp_p_ncpen_fun_",  (DL_FUNC) &_ncpen_native_cpp_p_ncpen_fun_,  14},
      {"_ncpen_native_cpp_qlasso_fun_",   (DL_FUNC) &_ncpen_native_cpp_qlasso_fun_,   10},
      {NULL, NULL, 0}
