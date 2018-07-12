2fe8cdfd598db1e1be1fc6f47a12ac06e5357420
Dongshin Kim <dongshin.kim@live.com>
2018-07-12 10:30:35 -0700

    2018 summer update

--- src/ncpen.h
+++ src/ncpen.h
@@ -1,8 +1,8 @@
 #include <RcppArmadillo.h>
 
 /*--------------------------------------------------------
- * Type definitions
- --------------------------------------------------------*/
+* Type definitions
+--------------------------------------------------------*/
 const int CONST_N_BIG_EXP_LOADER = 700;
 const double POI_BIG_XB = 700;
 const double LOGI_BIG_XB = 700;
@@ -33,9 +33,9 @@ public:
 };
 
 // Loss function proto types
-typedef double (*obj_fun_ptr)(arma::vec& y_vec, arma::mat& x_mat, arma::vec& b_vec, double r_eff);
-typedef arma::vec (*obj_grad_fun_ptr)(arma::vec& y_vec, arma::mat& x_mat, arma::vec& b_vec, double r_eff);
-typedef arma::mat (*obj_hess_fun_ptr)(arma::vec& y_vec, arma::mat& x_mat, arma::vec& b_vec, double r_eff);
+typedef double (*obj_fun_ptr)(arma::vec& y_vec, arma::mat& x_mat, arma::vec& b_vec);
+typedef arma::vec (*obj_grad_fun_ptr)(arma::vec& y_vec, arma::mat& x_mat, arma::vec& b_vec);
+typedef arma::mat (*obj_hess_fun_ptr)(arma::vec& y_vec, arma::mat& x_mat, arma::vec& b_vec);
 
 obj_fun_ptr get_obj_fun_ptr(std::string name);
 obj_grad_fun_ptr get_obj_grad_fun_ptr(std::string name);
@@ -52,14 +52,14 @@ int qlasso_fun(arma::mat& q_mat, arma::vec& l_vec, arma::vec& b_vec0, arma::vec&
                double lam, double iter_max, double b_eps, double k_eps, arma::uword p_eff, arma::uword q_rank, p_ncpen_ret& ret_buff);
 
 int p_ncpen_fun(arma::vec& y_vec, arma::mat& x_mat, arma::vec& b_vec0, arma::vec& w_vec,
-                double lam, double gam, double tau, double iter_max, double b_eps, double k_eps, arma::uword p_eff, double r_eff,
+                double lam, double gam, double tau, double alp, double iter_max, double b_eps, double k_eps, arma::uword p_eff,
                 obj_fun_ptr obj_fun, obj_grad_fun_ptr obj_grad_fun, obj_hess_fun_ptr obj_hess_fun,
                 pen_fun_ptr pen_fun, pen_grad_fun_ptr pen_grad_fun, p_ncpen_ret& ret_buff);
 
-int ncpen_fun(arma::vec& y_vec, arma::mat& x_mat0, bool x_std, bool intc,
-              arma::vec& w_vec0, arma::vec& lam_vec0, double r_lam,
-              double gam, double tau, arma::uword p_max, double iter_max, double b_eps, double k_eps,
-              arma::uword p_eff, double r_eff,
-              std::string fam, std::string pen, ncpen_ret& ret_buff);
-
+int ncpen_fun(arma::vec& y_vec, arma::mat& x_mat0,arma::vec& w_vec0, arma::vec& lam_vec0,
+              double gam, double tau, double alp, arma::uword d_max, double iter_max, double b_eps, double k_eps,
+              arma::uword p_eff,
+              std::string fam, std::string pen,
+              bool loc, arma::vec& ob_vec, int div,
+              ncpen_ret& ret_buff);
 
