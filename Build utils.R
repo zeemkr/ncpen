7699b9ef25a550073c287d6607c3c589b4155b7d
Dongshin Kim <dongshin.kim@live.com>
2018-08-22 13:27:26 -0700

    2018 summer upate #2

---
+++ Build utils.R
@@ -0,0 +1,32 @@
+# TODO ------------------------------------
+# coefficient names
+# intercept check
+# -----------------------------------------
+
+# Build mannual
+roxygen2::roxygenize('.', roclets=c('rd', 'collate', 'namespace', 'vignette'));
+outpath = paste(dirname(getwd()), "/doc/ncpen.pdf", sep = "");
+if(file.exists(outpath)) file.remove(outpath);
+system(paste("R CMD Rd2pdf -o ", outpath, " .", sep = ""));
+
+# To resolve dll registration warnings. -------------------------------
+# tools::package_native_routine_registration_skeleton(".");
+# This generates codes for init.c.
+# Run it and copy & paste to init.c
+tools::package_native_routine_registration_skeleton(".", character_only = FALSE)
+
+
+# To use Travis ----------------------------
+devtools::use_travis();
+
+
+# R CMD check
+#devtools::check();
+devtools::check(args = c("--use-valgrind"));
+#devtools::build_win();
+
+# Release
+devtools::release();
+
+devtools::submit_cran();
+
